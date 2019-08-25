358ad7b310bd2b840a48e82b28601cfed4001a6e
killeraliens <killeraliens@outlook.com>
2019-08-25 09:59:36 -0700

    filtered list onChange event

--- src/App.js
+++ src/App.js
@@ -7,32 +7,48 @@ class App extends Component {
     super(props);
 
     this.state = {
-      ailments: [],
-      selectedAilment: null
+      ailments: [
+        {
+          "problemID": 238,
+          "hcText": "Aches/Pains"
+        },
+        {
+          "problemID": 37,
+          "hcText": "Acne"
+        },
+        {
+          "problemID": 39,
+          "hcText": "Adrenal fatigue"
+        },
+        {
+          "problemID": 203,
+          "hcText": "Aging"
+        }
+      ]
     }
   }
 
-  componentDidMount() {
-    fetch('https://nutridigm-api-dev.azurewebsites.net/api/v1/nutridigm/healthconditions?subscriptionId=dd79cc82-f959-74f0-5fb4-f24082721083')
-    .then(resp => {
-      if(!resp.ok) {
-        throw new Error (resp.statusText)
-      }
-      return resp.json()
-    })
-    .then(respJson => {
-      console.log(respJson);
-      this.setState({
-        ailments: respJson,
-        error: null
-      });
-    })
-    .catch(err => {
-      this.setState({
-        error: err.message
-      })
-    })
-  }
+  // componentDidMount() {
+  //   fetch('https://nutridigm-api-dev.azurewebsites.net/api/v1/nutridigm/healthconditions?subscriptionId=dd79cc82-f959-74f0-5fb4-f24082721083')
+  //   .then(resp => {
+  //     if(!resp.ok) {
+  //       throw new Error (resp.statusText)
+  //     }
+  //     return resp.json()
+  //   })
+  //   .then(respJson => {
+  //     console.log(respJson);
+  //     this.setState({
+  //       ailments: respJson,
+  //       error: null
+  //     });
+  //   })
+  //   .catch(err => {
+  //     this.setState({
+  //       error: err.message
+  //     })
+  //   })
+  // }
 
   render() {
     const {ailments, selectedAilment, error} = this.state;
--- src/Autocomplete/Autocomplete.js
+++ src/Autocomplete/Autocomplete.js
@@ -1,24 +1,90 @@
 import React, {Component} from 'react';
+import PropTypes from 'prop-types';
 
 
 class Autocomplete extends Component {
+  static propTypes = {};
+  static defaultProps = {
+    ailments: []
+  }
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeSuggestion: 0,
+      filteredSuggestions: [],
+      showSuggestions: false,
+      userInput: ''
+    }
+  }
+
+  onChange = (e) => {
+    const filteredSuggestions = this.props.ailments.filter(ailment => ailment.hcText.toLowerCase().includes(e.target.value.toLowerCase()));
+    this.setState({
+      userInput: e.target.value,
+      filteredSuggestions: filteredSuggestions,
+      showSuggestions: true
+    })
+  }
+
   render() {
     const { ailments } = this.props;
-    const ailmentOptions = ailments.map(ailment => {
-      return <option key={ailment.problemID} id={ailment.problemID} value={ailment.hcText}>{ailment.hcText}</option>
-    });
+    const { activeSuggestion, filteredSuggestions, showSuggestions, userInput} = this.state;
+    const { onChange, onKeydown, onClick} = this;
 
-    return(
-      <div className='Autocomplete'>
-        <form>
-          <label htmlFor="ailmentInput">Enter health issue or problem area</label>
-          <select type="text" name="ailment input" id="ailmentInput" placeholder="lungs">
-            <option value="None">Select health issue or problem area</option>
-            {ailmentOptions}
-          </select>
-        </form>
-      </div>
+    let suggestionsListComponent;
+    if(userInput && showSuggestions) {
+      if(filteredSuggestions.length) {
+        suggestionsListComponent = (
+          <ul className="Autocomplete__suggestionsList">
+            {filteredSuggestions.map(ailment => {
+              return(
+                <li
+                  key={ailment.problemID}
+                  id={ailment.problemID}
+                  value={ailment.hcText}
+                  onClick={onClick}
+                >
+                  {ailment.hcText}
+                </li>
+              )
+            })}
+          </ul>
+        );
+      } else {
+        suggestionsListComponent = (
+          <div className="Autocomplete__noSuggestions">No Suggestions</div>
+        );
+      }
+    }
+
+    return (
+      <React.Fragment>
+        <label htmlFor="ailmentInput">Enter health issue or problem area</label>
+        <input
+          type="text"
+          id="ailmentInput"
+          name="ailment-input"
+          placeholder="lungs"
+          value={userInput}
+          onChange={onChange}
+          onKeydown={onKeydown}
+        />
+        {suggestionsListComponent}
+      </React.Fragment>
     );
+
+    // return(
+    //   <div className='Autocomplete'>
+    //     <form>
+    //       <label htmlFor="ailmentInput">Enter health issue or problem area</label>
+    //       <select type="text" name="ailment input" id="ailmentInput" placeholder="lungs">
+    //         <option value="None">Select health issue or problem area</option>
+    //         {ailmentOptions}
+    //       </select>
+    //     </form>
+    //   </div>
+    // );
   }
 }
 
