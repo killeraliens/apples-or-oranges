6bab77903545f7c8d56982d58d5be901d49f3ae3
killeraliens <killeraliens@outlook.com>
2019-08-25 23:44:56 -0700

    autocomplete compoent basics

--- src/App.js
+++ src/App.js
@@ -7,60 +7,55 @@ class App extends Component {
     super(props);
 
     this.state = {
-      ailments: [
-        {
-          "problemID": 238,
-          "hcText": "Aches/Pains"
-        },
-        {
-          "problemID": 37,
-          "hcText": "Acne"
-        },
-        {
-          "problemID": 39,
-          "hcText": "Adrenal fatigue"
-        },
-        {
-          "problemID": 203,
-          "hcText": "Aging"
-        }
-      ]
+      ailments: [],
+      results: [],
+      showForm: true
     }
   }
 
-  // componentDidMount() {
-  //   fetch('https://nutridigm-api-dev.azurewebsites.net/api/v1/nutridigm/healthconditions?subscriptionId=dd79cc82-f959-74f0-5fb4-f24082721083')
-  //   .then(resp => {
-  //     if(!resp.ok) {
-  //       throw new Error (resp.statusText)
-  //     }
-  //     return resp.json()
-  //   })
-  //   .then(respJson => {
-  //     console.log(respJson);
-  //     this.setState({
-  //       ailments: respJson,
-  //       error: null
-  //     });
-  //   })
-  //   .catch(err => {
-  //     this.setState({
-  //       error: err.message
-  //     })
-  //   })
-  // }
+  componentDidMount() {
+    fetch('https://nutridigm-api-dev.azurewebsites.net/api/v1/nutridigm/healthconditions?subscriptionId=dd79cc82-f959-74f0-5fb4-f24082721083')
+    .then(resp => {
+      if(!resp.ok) {
+        throw new Error (resp.statusText)
+      }
+      return resp.json()
+    })
+    .then(respJson => {
+      console.log(respJson);
+      this.setState({
+        ailments: respJson,
+        error: null
+      });
+    })
+    .catch(err => {
+      this.setState({
+        error: err.message
+      })
+    })
+  }
+
+  onSubmit = (e, activeAilmentId) => {
+    e.preventDefault();
+    console.log(activeAilmentId);
+    this.setState({
+      showForm: false
+
+    })
+  }
 
   render() {
-    const {ailments, selectedAilment, error} = this.state;
+    const {ailments, results, error, showForm} = this.state;
+    const {onSubmit} = this;
     const autocompleteComponent = ailments.length === 0
       ?  <div className="loading-div">Loading...</div>
-      :  <Autocomplete ailments={ailments}/>;
+      :  <Autocomplete ailments={ailments} onSubmit={onSubmit} showForm={showForm}/>;
     const errorComponent = error
       ?  <div className="error-div">{error}</div>
       : null;
     return (
       <div className="App">
-        {error}
+        {errorComponent}
         {autocompleteComponent}
       </div>
     );
--- src/Autocomplete/Autocomplete.js
+++ src/Autocomplete/Autocomplete.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
+import './Autocomplete.css';
 
 
 class Autocomplete extends Component {
@@ -14,38 +15,94 @@ class Autocomplete extends Component {
       activeSuggestion: 0,
       filteredSuggestions: [],
       showSuggestions: false,
-      userInput: ''
+      userInput: '',
+      activeAilmentId: null
     }
   }
 
   onChange = (e) => {
     const filteredSuggestions = this.props.ailments.filter(ailment => ailment.hcText.toLowerCase().includes(e.target.value.toLowerCase()));
     this.setState({
-      userInput: e.target.value,
+      activeSuggestion: 0,
+      userInput: e.currentTarget.value,
       filteredSuggestions: filteredSuggestions,
-      showSuggestions: true
+      showSuggestions: true,
+      activeAilmentId: null
     })
   }
 
+  onClick = (e) => {
+    console.log(e.currentTarget.value);
+    this.setState({
+      activeSuggestion: 0,
+      filteredSuggestions: [],
+      showSuggestions: false,
+      userInput: e.currentTarget.innerText,
+      activeAilmentId: e.currentTarget.value
+    })
+  }
+
+  onKeyDown = (e) => {
+    const { activeSuggestion, filteredSuggestions } = this.state;
+    if(e.keyCode === 13) {
+      console.log('enter key');
+      this.setState({
+        activeSuggestion: 0,
+        filteredSuggestions: [],
+        showSuggestions: false,
+        userInput: filteredSuggestions[activeSuggestion].hcText,
+        activeAilmentId: filteredSuggestions[activeSuggestion].problemID
+      });
+    } else if(e.keyCode === 40 && activeSuggestion < filteredSuggestions.length - 1) {
+      console.log('down key');
+      this.setState({
+        activeSuggestion: activeSuggestion + 1
+      })
+    } else if(e.keyCode === 38 && activeSuggestion > 0) {
+      console.log('up key');
+      this.setState({
+        activeSuggestion: activeSuggestion - 1
+      })
+    }
+  }
+
+  onSubmit = (e) => {
+    const { userInput, activeAilmentId } = this.state;
+    e.preventDefault();
+    this.props.onSubmit(e, activeAilmentId);
+    this.setState({
+      activeSuggestion: 0,
+      userInput: '',
+      filteredSuggestions: [],
+      showSuggestions: false,
+      activeAilmentId: null
+    })
+
+    // console.log('user input', {userInput});
+    // console.log('user input', {activeAilmentId});
+  }
+
   render() {
-    const { ailments } = this.props;
+    //const { ailments } = this.props;
     const { activeSuggestion, filteredSuggestions, showSuggestions, userInput} = this.state;
-    const { onChange, onKeydown, onClick} = this;
+    const { onChange, onKeyDown, onClick, onSubmit} = this;
 
     let suggestionsListComponent;
+
     if(userInput && showSuggestions) {
       if(filteredSuggestions.length) {
         suggestionsListComponent = (
           <ul className="Autocomplete__suggestionsList">
-            {filteredSuggestions.map(ailment => {
+            {filteredSuggestions.map((ailment, i) => {
               return(
                 <li
-                  key={ailment.problemID}
+                  key={i}
                   id={ailment.problemID}
-                  value={ailment.hcText}
+                  value={ailment.problemID}
                   onClick={onClick}
+                  className={activeSuggestion === i ? "active" : null}
                 >
-                  {ailment.hcText}
+                  <a href="#">{ailment.hcText}</a>
                 </li>
               )
             })}
@@ -60,17 +117,21 @@ class Autocomplete extends Component {
 
     return (
       <React.Fragment>
-        <label htmlFor="ailmentInput">Enter health issue or problem area</label>
-        <input
-          type="text"
-          id="ailmentInput"
-          name="ailment-input"
-          placeholder="lungs"
-          value={userInput}
-          onChange={onChange}
-          onKeydown={onKeydown}
-        />
-        {suggestionsListComponent}
+        <form action="" className="Autocomplete__form" autoComplete="off" onSubmit={onSubmit}>
+          <label htmlFor="ailmentInput">Enter health issue or problem area</label>
+          <input
+            type="text"
+            id="ailmentInput"
+            name="ailment-input"
+            placeholder="lungs"
+            value={userInput}
+            onChange={onChange}
+            onKeyDown={onKeyDown}
+
+          />
+          {suggestionsListComponent}
+          {/*<button type="submit">Submit</button>*/}
+        </form>
       </React.Fragment>
     );
 
